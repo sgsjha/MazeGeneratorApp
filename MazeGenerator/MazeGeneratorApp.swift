c04bd4c623095ad01b1e9542a237dae8d2d3d4cf
sgsjha <sarthakjhaa11@gmail.com>
2024-07-01 16:34:27 +0700

    Initial Commit

---
+++ MazeGenerator/MazeGeneratorApp.swift
@@ -0,0 +1,20 @@
+//
+//  MazeGeneratorApp.swift
+//  MazeGenerator
+//
+//  Created by Sarthak Jha on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MazeGeneratorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
